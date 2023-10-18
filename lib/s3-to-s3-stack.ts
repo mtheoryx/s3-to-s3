52ab3a29d95d80527191f5a5b4b722871d029efe
David Poindexter <drpoindexter@gmail.com>
2023-10-18 14:26:44 -0400

    Add basic 1:1 file flow

--- lib/s3-to-s3-stack.ts
+++ lib/s3-to-s3-stack.ts
@@ -1,7 +1,18 @@
 import * as cdk from "aws-cdk-lib";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
+import {
+  S3SourceProps,
+  S3Source,
+  S3InputFormat,
+  S3InputFileType,
+  S3OutputFileType,
+  S3Destination,
+  OnDemandFlow,
+  Mapping,
+} from "@cdklabs/cdk-appflow";
 import { Construct } from "constructs";
+import { S3 } from "aws-cdk-lib/aws-ses-actions";
 
 export class S3ToS3Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -31,6 +42,30 @@ export class S3ToS3Stack extends cdk.Stack {
     new s3deploy.BucketDeployment(this, "DeploySource", {
       sources: [s3deploy.Source.asset("./assets")],
       destinationBucket: sourceBucket,
+    });
+
+    // Appflow S3 Source Props and Source
+    const s3Source = new S3Source({
+      bucket: sourceBucket,
+      prefix: "cust",
+      format: {
+        type: S3InputFileType.CSV,
+      },
+    });
+
+    // Appflow S3 Destination Props and Destination
+    const s3Destination = new S3Destination({
+      location: { bucket: destinationBucket },
+      formatting: {
+        fileType: S3OutputFileType.CSV,
+      },
+    });
+
+    // Appflow Flow Definition
+    new OnDemandFlow(this, "OnDemandFlow", {
+      source: s3Source,
+      destination: s3Destination,
+      mappings: [Mapping.mapAll()], // Add empty mappings array
     });
   }
 }
