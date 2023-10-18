40fdfa381aae3df0a44d0cb36f68dfd43a1c8355
David Poindexter <drpoindexter@gmail.com>
2023-10-18 13:15:18 -0400

    Add source and destination buckets

--- lib/s3-to-s3-stack.ts
+++ lib/s3-to-s3-stack.ts
@@ -1,16 +1,29 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import { Construct } from "constructs";
 
 export class S3ToS3Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Source Bucket
+    const sourceBucket = new s3.Bucket(this, "sourceBucket", {
+      bucketName: "drp-sandbox-appelow-cdk-source",
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      publicReadAccess: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      autoDeleteObjects: true,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'S3ToS3Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Destination Bucket
+    const destinationBucket = new s3.Bucket(this, "destinationBucket", {
+      bucketName: "drp-sandbox-appelow-cdk-destination",
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      publicReadAccess: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      autoDeleteObjects: true,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+    });
   }
 }
